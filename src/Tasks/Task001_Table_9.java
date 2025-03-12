9f7f2bca91d496bf0f3bc2232646ed63eac44270
dipali1993 <dbdevchitte@gmail.com>
2025-03-13 00:52:37 +0530

    11th March

--- src/Tasks/Task001_Table_9.java
+++ src/Tasks/Task001_Table_9.java
@@ -1,4 +1,28 @@
 package Tasks;
 
 public class Task001_Table_9 {
+    public static void main(String[] args) {
+        int a = 9;
+        System.out.printf("---Table of 9--- %n");
+
+        System.out.printf("%d*1 = %d\n",a,a);
+
+        System.out.printf("%d*2 = %d\n",a,a*2);
+
+        System.out.printf("%d*3 = %d\n",a,a*3);
+
+        System.out.printf("%d*4 = %d\n",a,a*4);
+
+        System.out.printf("%d*5 = %d\n",a,a*5);
+
+        System.out.printf("%d*6 = %d\n",a,a*6);
+
+        System.out.printf("%d*7 = %d\n",a,a*7);
+
+        System.out.printf("%d*8 = %d\n",a,a*8);
+
+        System.out.printf("%d*9 = %d\n",a,a*9);
+
+        System.out.printf("%d*10 = %d",a,a*10);
+    }
 }
