8972b5fc5a2a4502e9d556a9829c7200a9841a6d
dipali1993 <dbdevchitte@gmail.com>
2025-04-06 20:38:52 +0530

    3rd April 2025- Assignments

---
+++ src/ex_19_OOps_Inheritance/hybrid/Lab180_Hybrid.java
@@ -0,0 +1,10 @@
+package ex_19_OOps_Inheritance.hybrid;
+
+public class Lab180_Hybrid {
+
+    //  Not possible by using classes
+}
+class GF{}
+class Father{}
+class Mother{}
+// class Son extends Father,Mother{}          not allowed
